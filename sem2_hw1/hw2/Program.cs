4d04f2029496f58a83f30643425f45fd97671aca
Ksenia <ksenia777555@mail.ru>
2022-11-22 20:59:03 +0300

    задача 2 не решена

---
+++ sem2_hw1/hw2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// вариант 1
+
+// Console.WriteLine("введите число: ");
+// num = Convert.ToInt32(Console.ReadLine());
+ //num1 = num1(num);
+// if (num1 > 3){
+//    num = num/Convert.ToInt32(Math.Pow(10, num1 - 3));
+// }
+// num = num%10;
+// Console.WriteLine($"третья цифра числа {num}");
+
+//вариант 2 (показали вначале урока, не работает)
+
+// Console.WriteLine("введите число: ");
+// string s = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(s[s.Length - 3].ToString());
+
+// вариант 3, сначала алгоритм сделать, потом перевести в С#, все равно не работает
+
+int ReadInt(string найти третью цифру){
+
+int number = WriteLine("Введите число: ");
+return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b);
+{
+    int result = 0;
+    if(b<3)
+    {
+        Console.Write("Третьей цифры нет: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i++){
+            c = c * 10;
+        }
+        result = (a / c) % 10;
+    }
+    return result;
+}
