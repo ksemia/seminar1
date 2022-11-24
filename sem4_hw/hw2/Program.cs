3421310fbef9380c9fac38ccfcc88a972ddedce9
Ksenia <ksenia777555@mail.ru>
2022-11-24 23:43:04 +0300

    2ая задача 4 семинар

---
+++ sem4_hw/hw2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+
+while (i > 0){
+    int num = i%10; i = i/10; summa = summa + num;
+}
+Console.WriteLine("сумма цифр в числе: " + summa);
