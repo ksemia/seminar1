9a9a17681f70d4ab71ef4c8d3a81e667057bd512
Ksenia <ksenia777555@mail.ru>
2022-11-24 20:00:58 +0300

    решена 2 задача 2ого семинара

--- sem2_hw1/hw2/Program.cs
+++ sem2_hw1/hw2/Program.cs
@@ -7,7 +7,7 @@
 
 // Console.WriteLine("введите число: ");
 // num = Convert.ToInt32(Console.ReadLine());
- //num1 = num1(num);
+//num1 = num1(num);
 // if (num1 > 3){
 //    num = num/Convert.ToInt32(Math.Pow(10, num1 - 3));
 // }
@@ -22,25 +22,52 @@
 
 // вариант 3, сначала алгоритм сделать, потом перевести в С#, все равно не работает
 
-int ReadInt(string найти третью цифру){
+// int ReadInt(string найти третью цифру){
 
-int number = WriteLine("Введите число: ");
-return Convert.ToInt32(Console.ReadLine());
-}
-int MakeArray(int a, int b);
+// int number = WriteLine("Введите число: ");
+// return Convert.ToInt32(Console.ReadLine());
+// }
+// int MakeArray(int a, int b);
+// {
+//    int result = 0;
+//    if(b<3)
+//    {
+//        Console.Write("Третьей цифры нет: ");
+//    }
+//    else
+//    {
+//        int c = 1;
+//        for (int i = b; i > 3; i++){
+//            c = c * 10;
+//        }
+//        result = (a / c) % 10;
+//    }
+//    return result;
+//}
+
+using static System.Console;
+
+//Console.WriteLine("введите число: ");
+//string s = Console.ReadLine();
+//Console.WriteLine(s[2].ToString());
+
+int ReadInt(string message)
 {
-    int result = 0;
-    if(b<3)
-    {
-        Console.Write("Третьей цифры нет: ");
-    }
-    else
-    {
-        int c = 1;
-        for (int i = b; i > 3; i++){
-            c = c * 10;
-        }
-        result = (a / c) % 10;
-    }
-    return result;
+    WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
+int CheckThirdNumber(int n)
+{
+    if (n < 100)
+    {
+        return -1;
+    }
+    for (int i = 10; n > 999;)
+    {
+        n /= i;
+    }
+    return n;
+}
+int n = ReadInt("Введите число");
+int answer = CheckThirdNumber(n);
+WriteLine(answer % 10);
